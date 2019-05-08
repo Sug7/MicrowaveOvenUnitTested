3f95cbb1e1d01f585f7a70381e4a2087db681d34
@Sug7 <sug-2508@hotmail.com>
2019-05-08 12:08:46 +0200

    More test to Step4

--- MicrowaveOven.Tests.Integration/Step4_CookControlerUserInterface.cs
+++ MicrowaveOven.Tests.Integration/Step4_CookControlerUserInterface.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using MicrowaveOvenClasses.Boundary;
 using MicrowaveOvenClasses.Controllers;
@@ -14,7 +15,7 @@ namespace MicrowaveOven.Tests.Integration
     [TestFixture]
     class Step4_CookControlerUserInterface
     {
-        private ICookController _cookController;
+        private CookController _cookController;
         private ITimer _timer;
         private IPowerTube _powerTube;
         private IDisplay _display;
@@ -40,7 +41,7 @@ namespace MicrowaveOven.Tests.Integration
             _door = Substitute.For<IDoor>();
             _light = Substitute.For<ILight>();
             _userInterface = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light, _cookController);
-            //_cookController.UI = _userInterface;
+            _cookController.UI = _userInterface;
         }
 
         [Test]
@@ -55,11 +56,31 @@ namespace MicrowaveOven.Tests.Integration
         [Test]
         public void UserInterface_OnPowerPressed2_Power()
         {
-            _powerButton.Press();
-            _powerButton.Press();
-            _timeButton.Press();
-            _startCancelButton.Press();
+            _powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            _powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            _timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            _startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
             _output.Received().OutputLine(Arg.Is<string>(str => str.Contains("100")));
+        }
+        [Test]
+        public void UserInterface_Stop_Power()
+        {
+            _powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            _timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            _startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            _startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            _output.Received().OutputLine(Arg.Is<string>(str => str.Contains("off")));
+        }
+
+        [Test]
+        public void UserInterface_OnTimerExpired_ClearDisplay()
+        {
+            _powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            _timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            _startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            Thread.Sleep(61000);
+
+            _display.Received().Clear();
         }
 
     }
